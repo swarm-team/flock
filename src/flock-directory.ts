4fbcbe84f62ff3d12e5342c53f7daa4722e7bfd2
FoxMoss <foxmoss@mediaology.com>
2023-04-29 14:17:46 -0500

    Sass support (import is untested)

--- src/flock-directory.ts
+++ src/flock-directory.ts
@@ -1,45 +1,68 @@
-import { abort } from './abort.ts';
+import { abort } from "./abort.ts";
+import sass from "https://deno.land/x/denosass/mod.ts";
 
 export type FlockDirectory = {
-	children: Record<string, FlockDirectory>;
-	src: string;
-	dependencies: string[];
+  children: Record<string, FlockDirectory>;
+  src: string;
+  css: string;
+  dependencies: string[];
 };
 
 export async function readFlockDirectory(path: string) {
-	try {
-		const directory: FlockDirectory = {
-			children: {},
-			src: '',
-			dependencies: [],
-		};
-		console.log(`Loading ${path}`);
-		// read index.js
-		try {
-			directory.src = await Deno.readTextFile(`${path}/index.js`);
-		} catch {
-			abort(`Directory "${path}" does not contain an index.js file.`);
-		}
-		// read deps
-		try {
-			directory.dependencies = JSON.parse(
-				await Deno.readTextFile(`${path}/deps.json`),
-			);
-		} catch {
-			// who cares
-		}
-		// read subdirectories
-		for await (const subdir of Deno.readDir(path)) {
-			if (subdir.isDirectory) {
-				// FIXME: Use a proper path operation instead of concatenation
-				directory.children[subdir.name] = await readFlockDirectory(
-					`${path}/${subdir.name}`,
-				);
-			}
-		}
-		return directory;
-	} catch {
-		abort(`Directory "${path}" does not exist. :/`);
-		throw new Error('Unreachable but typescript doesnt know that');
-	}
+  try {
+    const directory: FlockDirectory = {
+      children: {},
+      src: "",
+      css: "",
+      dependencies: [],
+    };
+    console.log(`Loading ${path}`);
+    // read index.js
+
+    try {
+      directory.src = await Deno.readTextFile(`${path}/index.js`);
+    } catch {
+      console.warn(
+        `Warning: Directory "${path}" does not contain an index.js file.`,
+      );
+      directory.src = "return {}";
+    }
+    try {
+      const scss = await Deno.readTextFile(`${path}/index.scss`);
+      const compiledSass = sass(scss).to_string();
+
+	  if(typeof compiledSass == "string")
+	  {
+		directory.css = compiledSass;
+	  }
+	  else
+	  {
+		abort("Error parsing SASS");
+	  }
+    } catch {
+      // guess there isnt a scss file
+    }
+
+    // read deps
+    try {
+      directory.dependencies = JSON.parse(
+        await Deno.readTextFile(`${path}/deps.json`),
+      );
+    } catch {
+      // who cares
+    }
+    // read subdirectories
+    for await (const subdir of Deno.readDir(path)) {
+      if (subdir.isDirectory) {
+        // FIXME: Use a proper path operation instead of concatenation
+        directory.children[subdir.name] = await readFlockDirectory(
+          `${path}/${subdir.name}`,
+        );
+      }
+    }
+    return directory;
+  } catch {
+    abort(`Directory "${path}" does not exist. :/`);
+    throw new Error("Unreachable but typescript doesnt know that");
+  }
 }
--- src/module-tree.ts
+++ src/module-tree.ts
@@ -1,58 +1,68 @@
-import { abort } from './abort.ts';
-import { DependencyResolver } from './dependency-resolver.ts';
-import { FlockDirectory } from './flock-directory.ts';
-import Uglify from 'npm:uglify-js';
+import { abort } from "./abort.ts";
+import { DependencyResolver } from "./dependency-resolver.ts";
+import { FlockDirectory } from "./flock-directory.ts";
+import Uglify from "npm:uglify-js";
 
 export type ModuleTreeNode = {
-	dependents: string[];
-	subNodes: Record<string, ModuleTreeNode>;
-	javascript: string;
-	name: string;
+  dependents: string[];
+  subNodes: Record<string, ModuleTreeNode>;
+  javascript: string;
+  name: string;
 };
 
 export class ModuleTreeConverter {
-	dependencyResolver: DependencyResolver;
+  dependencyResolver: DependencyResolver;
 
-	constructor() {
-		this.dependencyResolver = new DependencyResolver();
-	}
+  constructor() {
+    this.dependencyResolver = new DependencyResolver();
+  }
 
-	convertDirectory(dir: FlockDirectory) {
-		this.dependencyResolver.resolveDependencies(dir);
-		return this.createDirectory(dir, 'swarm');
-	}
+  convertDirectory(dir: FlockDirectory) {
+    this.dependencyResolver.resolveDependencies(dir);
+    return this.createDirectory(dir, "swarm");
+  }
 
-	private createDirectory(
-		dir: FlockDirectory,
-		moduleName: string,
-	): ModuleTreeNode {
-		const minified = Uglify.minify(dir.src, {
-			mangle: false,
-			parse: {
-				bare_returns: true,
-			},
-		});
+  private createDirectory(
+    dir: FlockDirectory,
+    moduleName: string,
+  ): ModuleTreeNode {
+	let injectedCSS = ``;
+    if (dir.css != "") {
+      injectedCSS +=
+        `const head = document.head || document.getElementsByTagName('head')[0];`;
+      injectedCSS += `const style = document.createElement('style');`;
+      injectedCSS += `style.appendChild(document.createTextNode("${dir.css}"));`;
+      injectedCSS += `style.type = 'text/css';`;
+	  injectedCSS += `head.appendChild(style);`;
+	  //console.log(injectedCSS);
+    }
+    const minified = Uglify.minify(injectedCSS + dir.src, {
+      mangle: false,
+      parse: {
+        bare_returns: true,
+      },
+    });
 
-		if (minified.error) {
-			abort(
-				`Failed to compile ${moduleName} during minification: ${minified.error}`,
-			);
-		}
+    if (minified.error) {
+      abort(
+        `Failed to compile ${moduleName} during minification: ${minified.error}`,
+      );
+    }
 
-		const output: ModuleTreeNode = {
-			dependents: this.dependencyResolver.modules[moduleName].dependents,
-			javascript: minified.code,
-			subNodes: {},
-			name: moduleName,
-		};
+    const output: ModuleTreeNode = {
+      dependents: this.dependencyResolver.modules[moduleName].dependents,
+      javascript: minified.code,
+      subNodes: {},
+      name: moduleName,
+    };
 
-		for (const subDir in dir.children) {
-			output.subNodes[subDir] = this.createDirectory(
-				dir.children[subDir],
-				`${moduleName}.${subDir}`,
-			);
-		}
+    for (const subDir in dir.children) {
+      output.subNodes[subDir] = this.createDirectory(
+        dir.children[subDir],
+        `${moduleName}.${subDir}`,
+      );
+    }
 
-		return output;
-	}
+    return output;
+  }
 }
